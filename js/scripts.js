1f5a0639c4a5bb7aa64175550df66e6d7f74ace5
younso9 <sonya.youngblood@verizon.com>
2019-07-25 15:08:24 -0400

    Final Updates/Corrections

--- js/scripts.js
+++ js/scripts.js
@@ -3,57 +3,38 @@
 //  *********Project 5 PUBLIC API REQUEST **************
 //  ****************************************************
 
-// fetch('https://randomuser.me/api/?results=12&nat=us')
-//     .then(response => response.json())
-//     .then(data => {
-//         console.log(data) // Prints result from `response.json()` in getRequest
-//     })
-//     .catch(error => console.error(error))
-
-// Random User Generator API link used to call 12 random US employees
-
-// $.ajax({
-//     type: 'GET',
-//     url: 'http://api.themoviedb.org/3/search/movie' + key + '&query=Minions',
-//     dataType: 'jsonp',
-//     data: {
-//         format: 'json'
-//     },
-//     error: $('#result').append("errore"),
-//     success: function (data) { $('#result').append("ok") }
-// });
-// var jsonData = data.results.original_title;
-//     //this give me a data is not provided
-// });
-
+// Random User Generator API link provided in HTML doc used to call 12 random US employees
 
 $.ajax({
     url: 'https://randomuser.me/api/?results=12&nat=us',
     dataType: 'json',
     success: function (data) {
         jsonData = data.results;
-        data.results.forEach(user => {           // Loops through employee data grabbing all JSON infomation
+
+        // This loops through the employee data grabbing all JSON infomation
+        data.results.forEach(user => {
 
             const picture = user.picture.large;
-            const firstName = user.name.first;
             const lastName = user.name.last;
+            const firstName = user.name.first;
             const email = user.email;
             const city = user.location.city;
             const state = user.location.state;
 
-            // This adds HTML ELEMENTS Dynamically 
+            // This adds HTML ELEMENTS Dynamically (copied from html document and manipulated)
             const galleryCard =
                 `<div class="card">
                 <div class="card-img-container">
                     <img class="card-img" src="${picture}" alt="Empl picture">
                 </div>
                 <div class="card-info-container">
-                    <h3 id="name" class="card-name cap">${firstName} ${lastName}</h3>
+                    <h3 id="name" class="card-name cap">${lastName}, ${firstName} </h3>
                     <p class="card-text">${email}</p>
                     <p class="card-text cap">${city}, ${state}</p>
                 </div>
                 </div>`;
-            // This will appends the HTML to the "gallery" id div tag
+
+            // This will appends the HTML element information to the "gallery" id div tag
             $('#gallery').append(galleryCard);
         })
     }
@@ -83,11 +64,9 @@ function modalWindow(i) {
     const state = jsonData[i].location.state;
     const zipCode = jsonData[i].location.postcode;
 
-    //This will reorder the DOB using the slice method - (Steve)
+    //This will reorder the DOB using the slice method - (Steve assisted)
     let dob = jsonData[i].dob.date;
     dob = dob.slice(5, 7) + '/' + dob.slice(8, 10) + '/' + dob.slice(0, 4);
-
-    // submit for review
 
     //This will add the HTML elements of the employee's info to the modal window pop up (copied from index.html)
     const modalContainer =
@@ -97,12 +76,11 @@ function modalWindow(i) {
                    <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
                    <div class="modal-info-container">
                        <img class="modal-img" src="${picture}" alt="Employee Picture">
-                       <h3 id="name" class="modal-name cap">${firstName}, ${lastName}</h3>
+                       <h3 id="name" class="modal-name cap">${firstName} ${lastName} <br> _____________________________</h3>
+                       <p>Contact Information:</p>
                        <p class="modal-text">${email}</p>
-                       <p class="modal-text cap">${city}</p>
-                       <hr>
                        <p class="modal-text">${phone}</p>
-                       <p class="modal-text cap">${street}, ${city}, ${state}, ${zipCode}</p>
+                       <p class="modal-text cap">${street}, <br>${city}, ${state} <br> ${zipCode}</p>
                        <p class="modal-text">Birthday: ${dob}</p>
                 </div>
         </div>`
